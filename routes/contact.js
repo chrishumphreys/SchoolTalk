5a6eec3e884e21089f263c327ac9e74191c303fb
chris <christian.humphreys@gmail.com>
2017-10-10 12:02:09 +0100

    contact page copy properties

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var  globals = require('../properties/globals.json');
+var  properties = require('../properties/contact.json');
 
 router.get('/', function(req, res, next) {
-  res.render('contact', {globals: globals});
+  res.render('contact', {st : properties, globals: globals});
 });
 
 module.exports = router;
