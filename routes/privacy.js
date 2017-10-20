74e6f2807025edbe680b335148541b66a0f58d48
chris <christian.humphreys@gmail.com>
2017-10-20 10:26:21 +0100

    tidy up privacy page

--- routes/privacy.js
+++ routes/privacy.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var  globals = require('../properties/globals.json');
+var  properties = require('../properties/privacy.json');
 
 router.get('/', function(req, res, next) {
-  res.render('privacy', {globals: globals});
+  res.render('privacy', {st: properties, globals: globals});
 });
 
 module.exports = router;
