2531ab74dee7646ed6d27633dcfe221aa94ee358
chris <christian.humphreys@gmail.com>
2017-10-21 23:52:14 +0100

    spike for sending email

--- email.js
+++ email.js
@@ -26,47 +26,51 @@ module.exports = {
 
         var template_path = path.join(__dirname, 'emails');
         var html_email = path.join(template_path, 'standard-html.hbs');
+        var text_email = path.join(template_path, 'standard-text.hbs');
 
         fs.readFile(html_email, 'utf-8', function(error, source){
             var template = handlebars.compile(source);
             var html = template(data);
-//            console.log("************************ sending email ***********************")
-            //console.log(html);
-//            console.log("**************************************************************")
             data.html = html;
 
-            var transporter = nodemailer.createTransport({
-                host: 'smtp.ethereal.email',
-                port: 587,
-                secure: false, // true for 465, false for other ports
-                auth: {
-                    user: process.env.SMTP_USER,
-                    pass: process.env.SMTP_PASS
-                }
+            fs.readFile(text_email, 'utf-8', function(error, source){
+                var template = handlebars.compile(source);
+                var text = template(data);
+                data.text = text;
+
+                var transporter = nodemailer.createTransport({
+                    host: 'smtp.ethereal.email',
+                    port: 587,
+                    secure: false, // true for 465, false for other ports
+                    auth: {
+                        user: process.env.SMTP_USER,
+                        pass: process.env.SMTP_PASS
+                    }
+                });
+
+                // setup email data with unicode symbols
+                var mailOptions = {
+                    from: data.from, // sender address
+                    to: data.to, // list of receivers
+                    subject: data.subject, // Subject line
+                    text: data.text, // plain text body
+                    html: data.html // html body
+                };
+
+                var callback = function(error, info) {
+                    if (error) {
+                        return console.log(error);
+                    }
+                    console.log('Message sent: %s', info.messageId);
+                    // Preview only available when sending through an Ethereal account
+                    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+                    debugSentEmailCallback(info.messageId, nodemailer.getTestMessageUrl(info));
+                };
+
+                transporter.sendMail(mailOptions, callback);
+
             });
-
-            // setup email data with unicode symbols
-            var mailOptions = {
-                from: data.from, // sender address
-                to: data.to, // list of receivers
-                subject: data.subject, // Subject line
-                text: data.text, // plain text body
-                html: data.html // html body
-            };
-
-            var callback = function(error, info) {
-                if (error) {
-                    return console.log(error);
-                }
-                console.log('Message sent: %s', info.messageId);
-                // Preview only available when sending through an Ethereal account
-                console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-                debugSentEmailCallback(info.messageId, nodemailer.getTestMessageUrl(info));
-            };
-
-            transporter.sendMail(mailOptions, callback);
-
         });
     }
 
